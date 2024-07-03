06994381df558689899685d9d94c5b316227ba19
vincent youmssi <vincentyoumssi@gmail.com>
2024-07-03 18:10:57 +0100

    positionate a theme button

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import { ModeToggle } from "./components/mode-toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 gap-4">
-      <ModeToggle />
+    <main className="relative flex min-h-screen flex-col items-center justify-center p-4 gap-4">
+      <div className="absolute right-4 top-4">
+        <ModeToggle />
+      </div>
       <section className="bg-white border-spacing-1 border w-full max-w-md max-h-96 h-full p-4 rounded-md"></section>
     </main>
   );
